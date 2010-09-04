3b6473064a0d54725735f139af0555c0d936f958
Fredy Treboux (liquidsnk) <liquidsnk@gmail.com>
2010-09-04 15:22:23 -0300

    First test (GetCount_WhenSubstringNotPresent_ReturnsZero) Red

---
+++ Source/TddPatternMatching/PatternMatcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TddPatternMatching
+{
+    public class PatternMatcher
+    {
+        public PatternMatcher(string text)
+        {
+        }
+
+        public int GetCount(string subString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
