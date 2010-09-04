442de70ef76725ab67208f8915c262a82aae0767
Fredy Treboux (liquidsnk) <liquidsnk@gmail.com>
2010-09-04 15:20:07 -0300

    First test (GetCount_WhenSubstringNotPresent_ReturnsZero) not compiling

---
+++ Source/TddPatternMatching.Tests/PatternMatcherTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TddPatternMatching.Tests
+{
+    [TestFixture]
+    public class PatternMatcherTests
+    {
+        [Test]
+        public void GetCount_WhenSubstringNotPresent_ReturnsZero()
+        {
+            var sut = new PatternMatcher("abra kadabra");
+            Assert.AreEqual(0, sut.GetCount("z"));
+        }
+    }
+}
