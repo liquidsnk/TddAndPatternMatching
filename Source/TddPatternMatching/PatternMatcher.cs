eba2327592ca4ebfc59dd1798036a09e42301fc4
Fredy Treboux (liquidsnk) <liquidsnk@gmail.com>
2010-09-04 15:30:03 -0300

    Second test (GetCount_WhenSubstringIsPresent_ReturnsCount) Green

--- Source/TddPatternMatching/PatternMatcher.cs
+++ Source/TddPatternMatching/PatternMatcher.cs
@@ -1,16 +1,26 @@
-﻿using System;
-
-namespace TddPatternMatching
+﻿namespace TddPatternMatching
 {
     public class PatternMatcher
     {
+        string text;
+
         public PatternMatcher(string text)
         {
+            this.text = text;
         }
 
         public int GetCount(string subString)
         {
-            return 0;
+            int count = 0;
+            foreach (var character in text)
+            {
+                if (character.ToString().Equals(subString))
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
